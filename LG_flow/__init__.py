233cfd7e78092b8a9956f962b1c4f013115cf25b
yatenglg <767624851@qq.com>
2025-10-17 14:41:30 +0800

    添加tensor的T()运算

--- LG_flow/__init__.py
+++ LG_flow/__init__.py
@@ -206,6 +206,10 @@ class Tensor:
         results = Math_op.LOG().forward([self])
         return Tensor(data=results.data, from_tensors=results.from_tensors, grad_fn=results.grad_fn)
 
+    def T(self):
+        results = Math_op.T().forward([self])
+        return Tensor(data=results.data, from_tensors=results.from_tensors, grad_fn=results.grad_fn)
+
     # 数据data原地改变后,更新属性
     def upattr(self):
         self.shape = self.data.shape
