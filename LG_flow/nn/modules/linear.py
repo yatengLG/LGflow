5644c8bc84997d9723c7e3279c239f6ad4303295
yatenglg <767624851@qq.com>
2025-10-20 16:50:47 +0800

    使用kaiming_uniform_初始化weight

--- LG_flow/nn/modules/linear.py
+++ LG_flow/nn/modules/linear.py
@@ -2,8 +2,10 @@
 # @Author  : LG
 
 from LG_flow.tensor import Tensor, randn, zeros
+from LG_flow.nn.init import kaiming_uniform_
 from .module import Parameter, Module
 from ..functional import linear
+import math
 
 
 class Linear(Module):
@@ -14,9 +16,13 @@ class Linear(Module):
         self.use_bias = use_bias
         self.weights = Parameter(randn(shape=(out_features, in_features), requires_grad=True))
         self.bias = Parameter(zeros(out_features,requires_grad=True)) if use_bias else None
+        self.reset_parameters()
 
     def forward(self, x:Tensor):
         return linear(x, self.weights, self.bias)
+
+    def reset_parameters(self):
+        kaiming_uniform_(self.weights)
 
     def __str__(self):
         return "LG_flow.nn.Linear(in_features={}, out_features={}, use_bias={})".format(self.in_features, self.out_features, self.use_bias)
