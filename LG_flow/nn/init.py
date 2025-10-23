81d614cca740351213470220b8b089d5b078c4c7
yatenglg <767624851@qq.com>
2025-10-23 11:32:18 +0800

    fix bug

--- LG_flow/nn/init.py
+++ LG_flow/nn/init.py
@@ -9,7 +9,7 @@ import numpy as np
 def kaiming_uniform_(tensor: Tensor, a: float = 0, mode='fan_in'):
     assert tensor.data.ndim == 2, "Only support 2-D tensor"
     fan = tensor.data.shape[1] if mode == 'fan_in' else tensor.data.shape[0]
-    gain = math.sqrt(2.0 / 1 + a ** 2) # for relu or leaky relu
+    gain = math.sqrt(2.0 / (1 + a ** 2)) # for relu or leaky relu
     std = gain / math.sqrt(fan)
     bound = math.sqrt(3.0) * std
     tensor.data = np.random.uniform(low=-bound, high=bound, size=tensor.data.shape)
