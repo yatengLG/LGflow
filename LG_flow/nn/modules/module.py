6c93d385a2f6e05479a89bf169182146372698f2
yatenglg <767624851@qq.com>
2026-02-11 16:11:39 +0800

    为Module类添加__call__函数，使可以直接调用，而不必调用forward函数。

--- LG_flow/nn/modules/module.py
+++ LG_flow/nn/modules/module.py
@@ -15,6 +15,9 @@ class Module(object):
     def forward(self, *inputs):
         raise NotImplementedError
 
+    def __call__(self, *inputs):
+        return self.forward(*inputs)
+
     def __setattr__(self, key, value):
         object.__setattr__(self, key, value)
 
