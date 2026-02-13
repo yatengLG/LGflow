254a63045cd51842751ba8fc3d8de39e0809ff11
yatenglg <767624851@qq.com>
2026-02-13 16:48:24 +0800

    添加concat与split调用方法

---
+++ LG_flow/functions.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author  : LG
+
+
+from .Math_op import Concat, Split
+from .tensor import Tensor
+
+
+def concat(tensors: list, axis=0):
+    assert isinstance(tensors, list)
+    results = Concat(axis).forward(tensors)
+    return Tensor(data=results.data, from_tensors=results.from_tensors, grad_fn=results.grad_fn)
+
+
+def split(tensor: Tensor, split_size_or_sections, axis=0):
+    assert isinstance(tensor, Tensor)
+    results = Split(split_size_or_sections, axis).forward([tensor])
+    return [Tensor(data=data, from_tensors=results.from_tensors, grad_fn=results.grad_fn, output_index=index)
+                for index, data in enumerate(results.data)]
+
